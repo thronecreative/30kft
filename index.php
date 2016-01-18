154973e77226c4dd4b1e041b416f032105c62d81
aimhighagency <bryan@aimhighagency.com>
2016-01-18 14:50:34 -0800

    added a splash page

--- index.php
+++ index.php
@@ -17,37 +17,9 @@
 	<body>
 
 		<div id="wrap">
-			 
-			<div id="nav-holder">
-				<div class="cta-header"><a href="
-http://30kftperformance.com.pages.ontraport.net/Welcome">Learn more now!</a></div>
-				<div id="menu-toggle">
-
-					<div class="icon-menu"></div>
-					<div class="icon-cross"></div>
-				</div>
-
-				<div id="nav">
-					<ul>
-						<li><a href="/#header">Welcome</a></li>
-						<li><a href="/#about">About 30KFT</a></li>
-						<li><a href="/#benefits">Member Benefits</a></li>
-						<li><a href="/#team">The Leadership</a></li>
-						<li><a href="/#contact" class="special">Contact Us</a></li>
-					</ul>
-
-					<div class="divider"></div>
-
-					<div class="social-icons">
-						<a href="http://twitter.com/" target="_blank" class="icon-twitter"></a>
-						<a href="http://facebook.com/" target="_blank" class="icon-facebook"></a>
-						<a href="http://instagram.com/" target="_blank" class="icon-instagram"></a>
-					</div>
-				</div>
-			</div>
 
 			<div id="main">
-				<section id="header">
+				 <section id="header">
 					<div id="logo"><img src="/images/30kft-Logo.svg" alt="30kft Logo" /></div>
 
 					<div class="inner">
@@ -57,284 +29,9 @@ http://30kftperformance.com.pages.ontraport.net/Welcome">Learn more now!</a></di
 					</div>
 
 					<div class="next">
-						<h3>What Else?</h3>
-						<div class="icon-arrow-down"></div>
+						<?php include('includes/cta.php'); ?>
 					</div>
 				</section>
-
-				<section id="slide-1" class="slide">
-					<div class="inner">
-						<h4> FACT </h4>
-						<h2>women should make as much as men.</h2>
-					</div>
-				</section>
-
-				<section id="about" class="image-bg">
-					<div class="inner">
-						<h1>About</h1>
-						<h2 class="pull">
-							EXPERIENCES MATTER.
-						</h2>
-						<div class="body">
-							
-							<h2>What we believe:</h2>
-
-							<ul>
-								<li>Your career does not have to suck</li>
-								<li>Business has the ability to solve complex American problems</li>
-								<li>Women should earn as much as men</li>
-								<li>High performance living is a science</li>
-								<li>Your network is your net worth</li>
-								<li>Wellness in business just makes sense</li>
-								<li>Healthy and wealthy mean the same thing</li>
-							</ul>
-							<div class="cta">If you agree. <a href="
-http://30kftperformance.com.pages.ontraport.net/Welcome">Learn More.</a></div>
-
-							
-
-							<h2>What we do:</h2>
-							<p>Our intensive workshops are invite-only and cater to those who seek fact-driven methods and techniques to take their lives and careers to 30,000 feet.  We help professionals on any career path, entrepreneurs and creatives UPGRADE. We offer 2, 3 and 4 day intensives that defy mental gravity. We help innovative, high growth companies TAKE OFF via our half day and full day bespoke performance programs.</p>
-							<p>Learn finance, fitness, wellness, rest and balance, acts of service, nutrition and more at the high performance level. Our content is 3rd party verified facts and not fiction. We give you clear and focused life and business hacks without all the noise that most summits and conferences provide.</p>
-							<p>We know you need quick and simple methods to improve your career and life.  College is over so welcome to the real world.</p>
-							<div class="divider"></div>
-							<?php include('includes/cta.php'); ?>
-							<div class="video">
-								<h2>Listen to our clients scream our praises!</h2>
-								<iframe src="https://player.vimeo.com/video/130911130?color=6891d1&title=0&byline=0&portrait=0" width="800" height="450" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-							</div>
-						</div>
-					</div>
-				</section>
-
-				<section id="slide-2" class="slide">
-					<div class="inner">
-						<h4> FACT </h4>
-						<h2>business can solve complex American problems</h2>
-					</div>
-				</section>
-
-				<section id="benefits" class="image-bg">
-					<div class="inner">
-						<h1>What you get</h1>
-						<h2 class="pull">Soar with us.</h2>
-						<div class="body">
-							<div class="side-left">
-								<h2>Membership Benefits</h2>
-								<ul>
-									<li>Discounts to all intensive workshops</li>
-									<li>Discounts for your family, friends and network via promo code</li>
-									<li>First Look at all our intensives before they go public</li>
-									<li>Free attendance to monthly socials via our partner + One</li>
-									<li>Our private mobile app to track your performance (June 2016)</li>
-									<li>Exclusive access to our network (It's who you know)</li>
-									<li>Amazing gifts (We love to spoil you)</li>
-									<li>(One complimentary hour) of wellness and career consulting</li>
-								</ul>
-								<div class="divider"></div>
-								<?php include('includes/cta-member.php'); ?>
-							</div>
-							
-							<div class="side-right">
-								<h2>Corporate Membership Benefits</h2>
-								<ul>
-									<li>Workforce performance audit</li>
-									<li>Quarterly check ins</li>
-									<li>Discounts on employee memberships</li>
-									<li>Discounts on employee intensive experiences</li>
-								</ul>
-								<div class="divider"></div>
-								<?php include('includes/cta.php'); ?>
-							</div>
-							<div class="video">
-								<h2>Watch how we make companies make more MULLAH through holistic business performance!</h2>
-								<iframe src="https://player.vimeo.com/video/120758561?color=6891d1&title=0&byline=0&portrait=0" width="800" height="450" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-							</div>
-						</div>
-					</div>
-				</section>
-
-				<section id="slide-2" class="slide">
-					<div class="inner">
-						<h4> FACT </h4>
-						<h2>health is wealth.</h2>
-					</div>
-				</section>
-
-				<section id="team" class="image-bg">
-					<div class="inner">
-						<h1>Leadership</h1>
-						<h2 class="pull">People matter.</h2>
-						<div class="body">
-							<div class="team">
-								<div class="member founder">
-									<div class="image"><img src="/images/30kft-james-kinney.jpg" alt="James N. Kinney" /></div>
-									<h2>James N. Kinney</h2>
-									<h3>Founder</h3>
-									<div class="info">
-									Former Wall St analyst, Law firm marketer and musician. <br />
-									Author of Living at 30KFT insights for post college America. <br />
-									Learned how to walk again at 16 and overcame anxiety holistically.
-									<div class="divider"></div>
-									</div>
-
-								</div>
-								<div class="member">
-									<div class="image"><img src="/images/30kft-dani-dwyer-willingham.jpg" alt="Dani Dwyer-Willingham" /></div>
-									<h2>Dani Dwyer-Willingham </h2>
-									<h3>Director of Marketing</h3>
-									<div class="info">
-									Expert in branding, marketing, content curation, and event facilitation.
-									Holistic health and wellness specialist, certified Reiki Master, and hypnotist.
-									Over ten years experience as an internationally successful model and actress.
-									</div>
-								</div>
-								<div class="member">
-									<div class="image"><img src="/images/30kft-rachel-ford.jpg" alt="JRachel Ford" /></div>
-									<h2>Rachel Ford</h2>
-									<h3>Director of Talent</h3>
-									<div class="info">
-										Licensed Clinical Social Worker, ten years of experience in the clinical field. 
-										Established practice and expert in training and mentoring therapist and interns. 
-										Certified in Integrative Behavioral Health and Cognitive Behavioral Therapy. 
-										Execution of work/life balance through roles as a wife, mother and successful professional.
-									</div>
-								</div>
-								<div class="member">
-									<div class="image"><img src="/images/30kft-mark-korotinsky.jpg" alt="Mark Korotinsky" /></div>
-									<h2>Mark Korotinsky </h2>
-									<h3>Director of Organizational Performance</h3>
-									<div class="info">
-										Industrial/Organizational Psychologist,  M.A. - I/O Psychology, Ph.D. - I/O Psychology in progress.
-										Expert in qualitative and quantitative methodologies and analytics.
-										Over ten years studying and teaching leadership, performance, alignment, and motivation.
-
-									</div>
-								</div>
-							</div>
-
-							<div class="divider center"></div>
-
-							<div class="consultants">
-								<h2 class="text-center title">Our Amazing Consultants:</h2>
-								<div class="member">
-									<h2>Name One</h2>
-									<h3>Consultant</h3>
-								</div>
-								<div class="member">
-									<h2>Name One</h2>
-									<h3>Consultant</h3>
-								</div>
-								<div class="member">
-									<h2>Name One</h2>
-									<h3>Consultant</h3>
-								</div>
-								<div class="member">
-									<h2>Name One</h2>
-									<h3>Consultant</h3>
-								</div>
-								<div class="member">
-									<h2>Name One</h2>
-									<h3>Consultant</h3>
-								</div>
-								<div class="member">
-									<h2>Name One</h2>
-									<h3>Consultant</h3>
-								</div>
-							</div>
-
-							<div class="advisors">
-							<h2 class="text-center title">Our Awesome Advisors:</h2>
-								<div class="member">
-									<h2>Kerianne Mellott</h2>
-									<h3>Director of Social Media eHarmony</h3>
-								</div>
-								<div class="member">
-									<h2>Kristen Ingram</h2>
-									<h3>Director of Marketing NFL Network</h3>
-								</div>
-								<div class="member">
-									<h2>Tyler Willingham</h2>
-									<h3>Global Marketing Manager </h3>
-								</div>
-								<div class="member">
-									<h2>Dana Sholl SPHR</h2>
-									<h3>Human Resources Director Zuma Hospitality</h3>
-								</div>
-							</div>
-							<div class="divider"></div>
-							<?php include('includes/cta.php'); ?>
-						</div>
-					</div>
-				</section>
-
-				<section id="slide-2" class="slide">
-					<div class="inner">
-						<h4> FACT </h4>
-						<h2>focus opens doors.</h2>
-					</div>
-				</section>
-
-				<section id="services" class="image-bg">
-					<div class="inner">
-						<h1>Services</h1>
-						<h2 class="pull">Lots to offer.</h2>
-						<div class="body">							
-							<h2 class="big">Private Wellness and Career Consulting</h2>
-							<p>Work with a certified 30KFT consultant after your intensive training to keep the momentum going. We offer flexible pricing and a myriad of experts that can help you lose weight, get a promotion, start a business and even run a marathon.</p>
-
-							<h3>Pay as you go no contract rate:</h3>
-							<ul>
-								<li><span class="rate">$150/hr</span>                       1 one hour session with no contract. </li>
-							</ul>
-							<h3>3 Month Subscription Rates:</h3>
-							<ul>
-								<li><span class="rate">$100</span>  One hour of consulting per month</li>
-								<li><span class="rate">$310</span>  Three hours of consulting per month</li>
-								<li><span class="rate">$625</span>  Four hours of consulting per month</li>
-								<li><span class="rate">$1250</span> 8 hours of consulting per month</li>
-								<li><span class="rate">$5000</span> 21 hours of team consulting per month</li>
-								<li>...and work with 3 consultants for 7 hours each per month</li>
-							</ul>
-
-							<h3>6 Month Subscription Rates:</h3>
-							<ul>
-								<li><span class="rate">$75</span>  One hour of consulting per month</li>
-								<li><span class="rate">$250</span>  Three hours of consulting per month</li>
-								<li><span class="rate">$500</span>  Four hours of consulting per month</li>
-								<li><span class="rate">$1000</span> 8 hours of consulting per month</li>
-								<li><span class="rate">$3750</span> 21 hours of team consulting per month</li>
-								<li>...and work with 3 consultants for 7 hours each per month</li>
-							</ul>
-							<div class="divider"></div>
-							<h2 class="big">TAKE OFF Intensive Workshops</h2>
-							<p>Multi-day experiences at luxury locations that deliver effective and powerful information.
-								Our members experience quick, tangible and provocative fact based solutions for optimal life and business performance.  Imagine yoga, meditation, money management and a concert in one place.</p>
-							<ul>
-								<li><span class="rate">$499</span>  2 day intensive Business Class</li>
-								<li><span class="rate">$999</span>  3 day intensive First Class</li>
-								<li><span class="rate">$1499</span>  4 day intensive Flying Private</li>
-								<li><span class="rate">$5000</span> Air Force One 10 day mastery course</li>
-							</ul>
-
-							<h3>For Companies</h3>
-							<p>In-office holistic business performance training. Lunch, half day or full day options available on with our innovative 30KFT curriculums that will be tailored to your company's specific needs. We offer highly engaging experiences that challenge traditional thinking.  Sound like your company?</p>
-							<ul>
-								<li><span class="rate">$2500</span>  Lunch training of up to 90 minutes</li>
-								<li><span class="rate">$5000</span>  half day options</li>
-								<li><span class="rate">$10000</span>  full day options</li>
-							</ul>
-						</div>
-					</div>
-				</section>
-
-				<section id="slide-2" class="slide">
-					<div class="inner">
-						<h4> FACT </h4>
-						<h2>High performance living is a science.</h2>
-					</div>
-				</section>
-
 				<section id="contact" class="image-bg">
 					<div class="inner">
 						<h1>Contact</h1>
@@ -368,21 +65,19 @@ http://30kftperformance.com.pages.ontraport.net/Welcome">Learn More.</a></div>
 						<h2>&copy; <?php echo date('Y'); ?> 30kft LLC. - Don't Steal. It's not nice.</h2>
 					</div>
 				</section>
-				
 			</div>
-
 			
 		</div>
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" ></script>
-		<script type="text/javascript" src="js/ScrollMagic.js"></script>
-		<script type="text/javascript" src="js/easing-min.js"></script>
-		<script type="text/javascript" src="js/TweenMax.js"></script>
-		<script type="text/javascript" src="js/debug.addIndicators.js"></script>
-		<script type="text/javascript" src="js/animation.gsap.js"></script>
-		<script type="text/javascript" src="js/jquery.fitvids.js"></script>
-		<script type="text/javascript" src="js/init.js"></script>
+		  ga('create', 'UA-72016127-1', 'auto');
+		  ga('send', 'pageview');
 
+		</script>
 	</body>
 
 </html>
